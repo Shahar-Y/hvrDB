823f70f5c3ba6fc2fd99cf49c15303e7d2d3ff03
Shahar Yair <shya95@gmail.com>
2023-08-11 17:22:56 +0300

    fix delivery change in db, 11-08-2023

--- src/types.ts
+++ src/types.ts
@@ -22,14 +22,8 @@ fetchData(
   "https://www.hvr.co.il/bs2/datasets/giftcard_branches.json",
   "./src/hvrDB/giftcard_branches.json"
 );
-fetchData(
-  "https://www.hvr.co.il/bs2/datasets/giftcard.json",
-  "./src/hvrDB/giftcard.json"
-);
-fetchData(
-  "https://www.mcc.co.il/bs2/datasets/mcccard.json",
-  "./src/hvrDB/mcccard.json"
-);
+fetchData("https://www.hvr.co.il/bs2/datasets/giftcard.json", "./src/hvrDB/giftcard.json");
+fetchData("https://www.mcc.co.il/bs2/datasets/mcccard.json", "./src/hvrDB/mcccard.json");
 fetchData(
   "https://www.mcc.co.il/bs2/datasets/mcccard_branches.json",
   "./src/hvrDB/mcccard_branches.json"
@@ -37,28 +31,28 @@ fetchData(
 
 import teamimBranches from "./hvrDB/teamimcard_branches.json";
 
-type TeamimStoreInfo = typeof teamimBranches.branch[0];
+type TeamimStoreInfo = (typeof teamimBranches.branch)[0];
 
 let teamimBranchesInfo: TeamimStoreInfo[] = teamimBranches.branch;
 
 import * as giftcardBranches from "./hvrDB/giftcard_branches.json";
-type giftcardBranchInfo = typeof giftcardBranches.ACE[0] & { company: string };
+type giftcardBranchInfo = (typeof giftcardBranches.ACE)[0] & { company: string };
 const giftcardBranchesDictionary: {
   [index: string]: Partial<giftcardBranchInfo>[];
 } = giftcardBranches;
 
 import giftcardCorps from "./hvrDB/giftcard.json";
-type giftcardCorpsInfo = typeof giftcardCorps[0];
+type giftcardCorpsInfo = (typeof giftcardCorps)[0];
 const giftcardCorpsArray: giftcardCorpsInfo[] = giftcardCorps;
 
 import * as mcccardBranches from "./hvrDB/mcccard_branches.json";
-type mcccardBranchInfo = typeof mcccardBranches.ACE[0];
+type mcccardBranchInfo = (typeof mcccardBranches.ACE)[0];
 const mcccardBranchesDictionary: {
   [index: string]: Partial<mcccardBranchInfo>[];
 } = mcccardBranches;
 
 import mcccardCorps from "./hvrDB/mcccard.json";
-type mcccardCorpsInfo = typeof mcccardCorps[0];
+type mcccardCorpsInfo = (typeof mcccardCorps)[0];
 const mcccardCorpsArray: mcccardCorpsInfo[] = mcccardCorps;
 
 type EnrichedStoreInfo = TeamimStoreInfo;
@@ -88,7 +82,7 @@ const teamimWriterArray = [
   { id: "address", title: "כתובת" },
   { id: "website", title: "אתר אינטרנט" },
   { id: "phone", title: "טלפון" },
-  { id: "is_delivery", title: "משלוחים" },
+  { id: "delivery", title: "משלוחים" },
   { id: "kosher", title: "כשרות" },
   { id: "handicap", title: "נגישות לנכים" },
   { id: "category", title: "קטגוריה" },
